37acee64e569ffaf08949aa7adaaffed83ffe4d0
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-06-16 09:32:05 +0900

    add-header-and-footer
    ヘッダーとフッターを追加（カテゴリーは表示されたまま）

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
+
 class Category < ApplicationRecord
+
+  has_ancestry
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   root "homes#index"
+
+  resources :categories
 
 end
