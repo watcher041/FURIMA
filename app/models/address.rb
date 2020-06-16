014b104292995123adbb7fe6261e40280063f9ee
watcher041 <63713236+watcher041@users.noreply.github.com>
2020-06-16 16:00:01 +0900

    edit-user-management
    ユーザー管理画面の装飾
    ユーザー登録の際のバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,7 @@
 class Address < ApplicationRecord
+
+  belongs_to :user, optional: true
+
+  validates :zipcode, :address, presence: true
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 
 Rails.application.routes.draw do
 
-  devise_for :users
+  # deviseで使用
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  # deviseにaddressパスを追加
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
 
   root "homes#index"
 
   resources :categories, only: [:index,:show]
+
+  resources :users
 
 end
